c20d8e2c424e4b4cbf2dd7fe55e59cd677010737
lostlevels <jimmy@jimmyp.email>
2015-10-21 15:06:17 -0700

    added some logging

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ var debug      = require('debug')('zipper')
 var s3 = new aws.S3()
 var bucket = process.env.AWS_BUCKET
 
+if (!bucket || !process.env.AWS_ACCESS_KEY_ID || !process.AWS_SECRET_ACCESS_KEY)
+  throw Error('Missing AWS fields')
+
 var app        = express()
 app.use(bodyParser.json())
 
@@ -41,6 +44,10 @@ app.get('/', function(req, res) {
     };
     archive.append(s3.getObject(opts).createReadStream(), {name: track.filename})
   });
+  archive.on('error', function(err) {
+    debug('archive error', err)
+    res.end('Error')
+  });
   archive.finalize()
 })
 
@@ -48,4 +55,7 @@ function attachment(filename) {
   return "attachment; filename=\"" + filename + '"'
 }
 
-app.listen(process.env.PORT || 5000)
+var port = process.env.PORT || 5000
+app.listen(port, function() {
+  debug('Server listening on port', port)
+});
