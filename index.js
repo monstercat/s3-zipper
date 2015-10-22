6ed33fc8f80a9ebe9462502586525949c1d30a93
lostlevels <jimmy@jimmyp.email>
2015-10-22 11:12:29 -0700

    cleanup, use post

--- index.js
+++ index.js
@@ -4,27 +4,26 @@ var bodyParser = require('body-parser')
 var aws        = require('aws-sdk')
 var debug      = require('debug')('zipper')
 
-var s3 = new aws.S3()
-var bucket = process.env.AWS_BUCKET
-
-if (!bucket || !process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY || !process.env.AWS_REGION)
+if (!process.env.AWS_BUCKET || !process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY || !process.env.AWS_REGION)
   throw Error('Missing AWS fields')
 
-var app        = express()
+var bucket = process.env.AWS_BUCKET
+var s3 = new aws.S3()
+var app = express()
 app.use(bodyParser.json())
 
-app.get('/', function(req, res) {
-  var filename = decodeURIComponent(req.query.filename)
-    , tracks = decodeURIComponent(req.query.tracks)
-    , userId = req.query.userId
+app.post('/', function(req, res) {
+  var filename = req.body.filename
+    , tracks = req.body.tracks
+    , userId = req.body.userId
 
   if (!filename || !tracks || !userId)
-    return res.status(404).send('Missing fields.')
+    return res.status(400).send('Missing fields.')
 
   try{
     tracks = JSON.parse(tracks)
   } catch(e) {
-    return res.status(404).send('Invalid tracks.')
+    return res.status(400).send('Invalid tracks.')
   }
 
   debug('Writing playlist', filename)
@@ -35,14 +34,15 @@ app.get('/', function(req, res) {
   })
 
   var archive = archiver.create('zip', {store: true})
-  archive.on('error', function(err) {
+  .on('error', function(err) {
     debug('archive error', err)
     res.end('Error')
-  }).
-  on('finish', function() {
+  })
+  .on('finish', function() {
     debug('Finished writing', filename)
-  }).
-  pipe(res)
+  })
+
+  archive.pipe(res)
 
   tracks.forEach(function zipTrack(track) {
     var opts = {
@@ -50,7 +50,7 @@ app.get('/', function(req, res) {
       Key: track.key
     };
     archive.append(s3.getObject(opts).createReadStream(), {name: track.filename})
-  });
+  })
   archive.finalize()
 })
 
@@ -61,4 +61,4 @@ function attachment(filename) {
 var port = process.env.PORT || 5000
 app.listen(port, function() {
   debug('Server listening on port', port)
-});
+})
